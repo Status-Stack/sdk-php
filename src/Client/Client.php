07848fcd7bbbf6ad5c7738f130d9ee437a73f607
Lukáš Hron <info@lukashron.cz>
2022-10-02 21:17:11 +0200

    Code copyright

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the Status Stack package.
+ *
+ * (c) Lukas Hron <info@lukashron.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace StatusStack\Client;
 
 use Psr\Http\Client\ClientInterface;
